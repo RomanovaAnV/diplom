71e611aea8a182a67dbb500a9745a17aff1ea174
Chuchko Dobronrav <cdobronrav@mail.ru>
2021-04-15 16:35:42 +0300

    test json info upload

--- frontend/app/form.js
+++ frontend/app/form.js
@@ -1,8 +1,14 @@
-function upload_data(files,link){
-upload={
-  face_files:files.push(0,3),
+function clickHandler(){
+files=document.getElementById("files").value;
+link=document.getElementById("link").value;
+upload_data={
+  face_files:files,
   album_link:link
 }
-json=JSON.stringify(upload)
-alert(json)
-};
+json=JSON.stringify(upload_data)
+  var url = 'https://findmychild.com/new_request'
+  var xhr = new XMLHttpRequest()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+  xhr.send(json)
+}
