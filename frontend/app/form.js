7308ba57d5e7cb94d71870f529f9d229145001db
Chuchko Dobronrav <cdobronrav@mail.ru>
2021-04-15 03:51:02 +0300

    PROBLEMS WITH FORM UPLOAD

--- frontend/app/form.js
+++ frontend/app/form.js
@@ -1,66 +1,8 @@
-let dropArea = document.getElementById("drop-area")
-// Prevent default drag behaviors
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)   
-  document.body.addEventListener(eventName, preventDefaults, false)
-})
-
-// Highlight drop area when item is dragged over it
-;['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
-
-// Handle dropped files
-dropArea.addEventListener('drop', handleDrop, false)
-
-function preventDefaults (e) {
-  e.preventDefault()
-  e.stopPropagation()
+function upload_data(files,link){
+upload={
+  face_files:files.push(0,3),
+  album_link:link
 }
-
-function highlight(e) {
-  dropArea.classList.add('highlight')
-}
-
-function unhighlight(e) {
-  dropArea.classList.remove('active')
-}
-
-function handleDrop(e) {
-  var dt = e.dataTransfer
-  var files = dt.files
-  handleFiles(files)
-}
-
-function handleFiles(files) {
-  files = [...files]
-  uploadFiles(files)
-  files.forEach(previewFile)
-}
-
-function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
-}
-
-
-function uploadFiles(files) {
-  var API = 'http://www.findmychild/upload'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', API, true)
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-  // Update progress (can be used to show progress indicator)
-  formData.append('file',files.slice(0,3))
-  xhr.send(formData)
-}
+json=JSON.stringify(upload)
+alert(json)
+};
